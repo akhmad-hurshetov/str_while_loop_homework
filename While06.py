cdc1337bb9741247aa4c1123865d44554fa756e2
Akhmad <akhmadhurshetov@gmail.com>
2022-11-09 03:41:37 +0500

    06-two ways

--- While06.py
+++ While06.py
@@ -17,3 +17,22 @@ def main(s):
             count+=1
         i+=1
     return count
+print(main('helloworld! @'))
+
+
+
+'''
+def main(s):
+    n=len(s)
+    i=0
+    count=0
+    s=s.lower()
+    vowels='aeiou'
+    while i<n:
+        x=s[i]
+        if vowels.find(x)==-1:
+            count+=1
+        i+=1
+    return count
+print(main('absec'))
+'''
